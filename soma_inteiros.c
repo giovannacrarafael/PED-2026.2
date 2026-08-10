f2fe9c9771d2534d8bff4356875b7f9ad0c76fb5
giovannacrarafael <giovannacrarafael@gmail.com>
2026-08-10 19:38:57 -0300

    Add files via upload

---
+++ soma_inteiros.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 2;
+    int b = 15;
+    printf("%d", a + b);
+
+    return 0;
+}
